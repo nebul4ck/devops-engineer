f3dc318a54be68a0bc6a64cf894531adc60a1fea
nebul4ck <a.gonzalezmesas@gmail.com>
2021-06-03 20:01:25 +0200

    improve filemanager generator name files adding jpg and png extensions

--- scripts/sysadmin/filenamegenerator.py
+++ scripts/sysadmin/filenamegenerator.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #-*- encoding: utf-8 -*-
 
 from pathlib import Path
 
 import uuid
 import os
+import re
 
-path = ""
+path = "."
 
 def ls3(path):
     for file in Path(path).iterdir():
@@ -14,10 +15,21 @@ def ls3(path):
 
             newuuid = str(uuid.uuid4())
             uuidname = newuuid.split('-',)[4]
-            newfile = path+'/'+uuidname+'.jpg'
 
-            print file
-            os.rename(str(file),newfile)
-            print newfile
+            print(file)
+
+            file_name, file_ext = os.path.splitext(file)
+
+            if re.match('^.[Jj][Pp][Gg]?', file_ext):
+
+                newfile = path+'/'+uuidname+'.jpg'
+                os.rename(str(file),newfile)
+
+            elif re.match('^.[Pp][Nn][Gg]', file_ext):
+
+                newfile = path+'/'+uuidname+'.png'
+                os.rename(str(file),newfile)
+
+            print(newfile)
 
 ls3(path)
